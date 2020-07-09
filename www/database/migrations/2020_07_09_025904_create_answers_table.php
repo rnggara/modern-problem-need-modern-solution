0d4d1cb394bb50abb7b59f97ac404be5d56747dd
rnggara <ranggaanggara8@gmail.com>
2020-07-09 15:59:55 +0700

    init

---
+++ www/database/migrations/2020_07_09_025904_create_answers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->integer('pinned');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_thread')->nullable();
+            $table->timestamps();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_thread')->references('id')->on('threads');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
---
+++ www/database/migrations/2020_07_09_044740_create_votes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('vote');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_thread')->nullable();
+            $table->unsignedBigInteger('id_answer')->nullable();
+            $table->timestamps();
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_thread')->references('id')->on('threads');
+            $table->foreign('id_answer')->references('id')->on('answers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
