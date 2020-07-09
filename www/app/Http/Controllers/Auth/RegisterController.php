f1a5d2104282541103ba884a0c2d49ae51d40108
Wahyu Setya Permadi <wsetyapermadimobile@gmail.com>
2020-07-09 21:56:12 +0700

    add username field to registration

--- www/app/Http/Controllers/Auth/RegisterController.php
+++ www/app/Http/Controllers/Auth/RegisterController.php
@@ -64,8 +64,10 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // dd($data);
         return User::create([
             'name' => $data['name'],
+            'username' => $data['username'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
